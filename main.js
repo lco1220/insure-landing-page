b3b7b7cfd56cdc515b616f9f388242a5c2fe20b7
lco1220 <leorieleco@gmail.com>
2020-06-17 15:49:46 +0800

    transition mobile nav

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 let mobile_btn = document.getElementById('mobile__btn');
+let close_btn = document.getElementById('cross');
+let def_btn = document.getElementById('threelines')
 let mobile_nav = document.getElementById('mobile--menu');
 let body = document.getElementById('body');
 
-
 mobile_btn.addEventListener('click', () => {
-  mobile_nav.classList.toggle('active'),
+  mobile_nav.classList.toggle('active')
   body.classList.toggle('overflow_hidden')
+
+  if(mobile_nav.classList.contains('active')) {
+
+    close_btn.classList.add('opacity_show');
+    close_btn.classList.remove('opacity_noshow');
+    def_btn.classList.add('opacity_noshow');
+    def_btn.classList.remove('opacity_show');
+  } else {
+
+    def_btn.classList.add('opacity_show');
+    def_btn.classList.remove('opacity_noshow');
+    close_btn.classList.add('opacity_noshow');
+    close_btn.classList.remove('opacity_show');
+  }
 
 })
